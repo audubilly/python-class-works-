430f1837397a324f5e4da9c23e97a48a3b15a6e7
Audu Billy <audubilly89@gmail.com>
2021-01-30 04:02:12 +0100

    updated

--- add_class/addition.py
+++ add_class/addition.py
@@ -16,3 +16,13 @@ def sub(num1: float, num2: float) -> float:
 def div(num1: float, num2: float) -> float:
     num3 = num1 / num2
     return num3
+
+
+def sqr(num1: float) -> float:
+    num2 = num1 ** 2
+    return num2
+
+
+def sqrt(num1: float) -> float:
+    num2 = num1 ** 0.5
+    return num2
