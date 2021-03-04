36c44eb1b5f9060496c613f285ecf4012821d33a
Audu Billy <audubilly89@gmail.com>
2021-03-04 15:52:57 +0100

    first commit

---
+++ Even_Odd/Even_or_Odd.py
@@ -0,0 +1,8 @@
+def even_or_odd(arg1):
+    if arg1 % 2 == 0:
+        print("number is even")
+    else:
+        print("number is odd")
+
+
+even_or_odd(8)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# python-class-works-" 
+"# python_classworks" 
