ddba0cb52d1fd8259a26f0cfd0e53efd839ebbb8
Audu Billy <audubilly89@gmail.com>
2021-03-15 15:32:27 +0100

    updates

--- classWork/days_of_a_week.py
+++ classWork/days_of_a_week.py
@@ -1,14 +1,28 @@
-my_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-tasks = ["java", "sleep", "Python", "Data Science", "Catch cruise", "Flex", "Flex"]
+# my_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# tasks = ["java", "sleep", "Python", "Data Science", "Catch cruise", "Flex", "Flex"]
+#
+# # print(dict(zip(my_week, tasks)))
+# zip(my_week, tasks)
+#
+#
+# def make_daily_plans(days_of_week, tasks):
+#     my_daily_plans = {}
+#     for x, y in zip(my_week, tasks):
+#         my_daily_plans[x] = y
+#
+#
+# make_daily_plans()
 
-# print(dict(zip(my_week, tasks)))
-zip(my_week, tasks)
+
+def square(squares, values):
+    result = {}
+    for x, y in zip(squares, values):
+        result[x] = y * y
+    return result
 
 
-def make_daily_plans(days_of_week, tasks):
-    my_daily_plans = {}
-    for x, y in zip(my_week, tasks):
-        my_daily_plans[x] = y
+squares = ["Ones", "Two", "Three", "Four", "Five"]
+values = [1, 2, 3, 4, 5]
 
-
-make_daily_plans()
+print(square(squares, values))
+# print(dict(zip(squares, (values * values))))
